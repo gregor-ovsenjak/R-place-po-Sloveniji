36c55c3aff74e200ef1e81eb6ddabc6c9b498393
Gregor Ovsenjak <gregor.ovsenjak@gmail.com>
2021-06-30 09:54:22 +0200

    Začel z analizo podatkov

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,27 @@
-# 4. faza: Analiza podatkov
+library("caret")
+library("recipes")
+library("ggplot")
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+place_po_sektorjih %>% View
+place_po_sektorjih %>% filter(IZOBRAZBA != "Izobrazba - Skupaj") %>%
+  ggplot(aes(x=POVPRECNA_PLACA, fill=factor(IZOBRAZBA))) + 
+  geom_histogram(binwidth=100)
+
+
+inTrain <- createDataPartition(
+  y = place_po_sektorjih$POVPRECNA_PLACA,
+  ## the outcome data are needed
+  p = .75,
+  ## The percentage of data in the
+  ## training set
+  list = FALSE
+)
+
+training <- place_po_sektorjih[ inTrain,]
+testing  <- place_po_sektorjih[-inTrain,]
+
+
+preProcessValues <-preProcess(training,method = c("BoxCox","center","scale"))
+trainTransformed <- predict(preProcessValues, training)
+trainTransformed
